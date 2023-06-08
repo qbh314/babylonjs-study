7831bd5802676f8a19461cffc75fde4a1cbf3576
qbh314 <15979005807@163.com>
2023-06-08 20:10:38 +0800

    feat: 实现第一个3d页面的渲染

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,56 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// 导入babylonjs
+import * as BABYLON from 'babylonjs'
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// 创建canvas
+const canvas = document.createElement('canvas')
+// 设置canvas宽高
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+// 将canvas添加到body中
+document.body.appendChild(canvas)
+
+// 创建引擎 
+const engine = new BABYLON.Engine(canvas, true)
+
+// 创建场景
+const scene = new BABYLON.Scene(engine)
+
+// 创建相机
+const camera = new BABYLON.ArcRotateCamera(
+  'camera', // 相机名称
+  0, // 相机的alpha值，水平旋转角度
+  0, // 相机的alpha值，垂直旋转角度
+  10, // 相机的半径
+  BABYLON.Vector3.Zero(), // 相机的目标点
+  scene // 相机的场景
+)
+
+// 把相机附加到画布上
+camera.attachControl(canvas, true)
+
+// 创建球
+const sphere = BABYLON.MeshBuilder.CreateSphere(
+  'sphere', // 球的名称
+  { diameter: 2 }, // 球的直径
+  scene, // 球所在的场景
+)
+
+// 创建光源
+const light = new BABYLON.DirectionalLight(
+  'light', // 光源名称
+  new BABYLON.Vector3(0, -1, 0), // 光源的方向
+  scene, // 光源所在的场景
+)
+
+// 渲染场景
+engine.runRenderLoop(() => {
+  scene.render()
+})
+
+// 监听窗口变化
+window.addEventListener('resize', () => {
+  engine.resize()
+})
+
